6ec251d230d313f8898d40c9ddd3e6309b5b8713
nflsgl <alecbonifer@gmail.com>
2025-07-23 06:05:14 -0400

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,66 @@
+const schedule = {
+  "1": [
+    {
+      "home": "Philadelphia Eagles",
+      "away": "Dallas Cowboys",
+      "date": "2025-09-04",
+      "odds": {
+        "moneyline": {
+          "home": -325,
+          "away": 260
+        },
+        "spread": {
+          "home": -7,
+          "away": 7
+        }
+      }
+    },
+    {
+      "home": "Kansas City Chiefs",
+      "away": "Los Angeles Chargers",
+      "date": "2025-09-05",
+      "odds": {
+        "moneyline": {
+          "home": -148,
+          "away": 124
+        },
+        "spread": {
+          "home": -3,
+          "away": 3
+        }
+      }
+    },
+    {
+      "home": "Minnesota Vikings",
+      "away": "Chicago Bears",
+      "date": "2025-09-08",
+      "odds": {
+        "moneyline": {
+          "home": -110,
+          "away": -106
+        },
+        "spread": {
+          "home": -1.5,
+          "away": 1.5
+        }
+      }
+    }
+  ],
+  "2": [],
+  "3": [],
+  "4": [],
+  "5": [],
+  "6": [],
+  "7": [],
+  "8": [],
+  "9": [],
+  "10": [],
+  "11": [],
+  "12": [],
+  "13": [],
+  "14": [],
+  "15": [],
+  "16": [],
+  "17": [],
+  "18": []
+};
